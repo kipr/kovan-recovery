ad0c0d790a5b17665b7e71487ed17aea6413fcc8
Braden McDorman <bmcdorman@gmail.com>
2012-11-21 14:08:01 -0600

    Added string.h include for strerror function

--- log.h
+++ log.h
@@ -2,17 +2,12 @@
 #define _LOG_H_
 
 #include <stdio.h>
-
-#ifndef __linux__
-#define LOG_PERROR(format, arg...) do {} while(0)
-
-#else
+#include <string.h>
 
 #define LOG_PERROR(format, arg...) \
 	do { \
 		fprintf(stderr, "%s - %s():%d - " format ": %s\n", __FILE__, __func__, __LINE__, ## arg, strerror(errno)); \
 	} while(0)
-#endif
 
 #define LOG_ERROR(format, arg...) \
 	do { \
